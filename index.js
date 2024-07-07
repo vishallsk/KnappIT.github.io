0ea15dbf85c37bea3d7054142b0fe0e4171e9a56
vishallsk <144356311+vishallsk@users.noreply.github.com>
2024-07-07 09:01:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const sr= ScrollReveal ({
+    distance:'65px',
+    duration: 2600,
+    delay:450,
+    reset:true,
+});
+sr.reveal('.hero-text',{delay:200,origin:'top'});
+sr.reveal('.hero-img',{delay:450,origin:'top'});
+sr.reveal('.icons',{delay:550,origin:'left'});
