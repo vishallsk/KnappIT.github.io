19d700404a39fd9403f60cc94af36210954520a6
vishallsk <144356311+vishallsk@users.noreply.github.com>
2024-07-08 21:44:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const sr= ScrollReveal ({
-    distance:'65px',
-    duration: 2600,
-    delay:450,
-    reset:true,
+document.getElementById('menu-icon').addEventListener('click', function() {
+    document.querySelector('.navlist').classList.toggle('open');
 });
-sr.reveal('.hero-text',{delay:200,origin:'top'});
-sr.reveal('.hero-img',{delay:450,origin:'top'});
-sr.reveal('.icons',{delay:550,origin:'left'});
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true,
+});
+sr.reveal('.hero-text', { delay: 200, origin: 'top' });
+sr.reveal('.hero-img', { delay: 450, origin: 'top' });
+sr.reveal('.icons', { delay: 550, origin: 'left' });
